cd1e67e836107134143bac6a5ebc00db47e45f11
dmilner@gmail.com <dmilner@gmail.com@5099ddc8-4009-11de-952f-710615d9d0d5>
2011-05-17 19:36:05 +0000

    Updated text for new version. Fixed conversion type errors.
    git-svn-id: http://netprofiles.googlecode.com/svn/trunk@19 5099ddc8-4009-11de-952f-710615d9d0d5

--- About.Designer.vb
+++ About.Designer.vb
@@ -33,75 +33,73 @@ Partial Class About
 	''' not be able to load this method if it was changed manually.
 	''' </summary>
 	Private Sub InitializeComponent()
-		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(About))
-		Me.pictureBox1 = New System.Windows.Forms.PictureBox
-		Me.label1 = New System.Windows.Forms.Label
-		Me.labelVersion = New System.Windows.Forms.Label
-		Me.labelCopyright = New System.Windows.Forms.Label
-		Me.buttonOK = New System.Windows.Forms.Button
-		Me.linkLabelHomepage = New System.Windows.Forms.LinkLabel
-		CType(Me.pictureBox1,System.ComponentModel.ISupportInitialize).BeginInit
-		Me.SuspendLayout
-		'
-		'pictureBox1
-		'
-		resources.ApplyResources(Me.pictureBox1, "pictureBox1")
-		Me.pictureBox1.Name = "pictureBox1"
-		Me.pictureBox1.TabStop = false
-		'
-		'label1
-		'
-		resources.ApplyResources(Me.label1, "label1")
-		Me.label1.Name = "label1"
-		'
-		'labelVersion
-		'
-		resources.ApplyResources(Me.labelVersion, "labelVersion")
-		Me.labelVersion.Name = "labelVersion"
-		'
-		'labelCopyright
-		'
-		resources.ApplyResources(Me.labelCopyright, "labelCopyright")
-		Me.labelCopyright.Name = "labelCopyright"
-		'
-		'buttonOK
-		'
-		resources.ApplyResources(Me.buttonOK, "buttonOK")
-		Me.buttonOK.Name = "buttonOK"
-		Me.buttonOK.UseVisualStyleBackColor = true
-		AddHandler Me.buttonOK.Click, AddressOf Me.ButtonOKClick
-		'
-		'linkLabelHomepage
-		'
-		resources.ApplyResources(Me.linkLabelHomepage, "linkLabelHomepage")
-		Me.linkLabelHomepage.Name = "linkLabelHomepage"
-		Me.linkLabelHomepage.TabStop = true
-		AddHandler Me.linkLabelHomepage.LinkClicked, AddressOf Me.LinkLabelHomepageLinkClicked
-		'
-		'About
-		'
-		resources.ApplyResources(Me, "$this")
-		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.Controls.Add(Me.pictureBox1)
-		Me.Controls.Add(Me.linkLabelHomepage)
-		Me.Controls.Add(Me.buttonOK)
-		Me.Controls.Add(Me.labelCopyright)
-		Me.Controls.Add(Me.labelVersion)
-		Me.Controls.Add(Me.label1)
-		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
-		Me.MaximizeBox = false
-		Me.MinimizeBox = false
-		Me.Name = "About"
-		Me.ShowInTaskbar = false
-		AddHandler Load, AddressOf Me.AboutLoad
-		CType(Me.pictureBox1,System.ComponentModel.ISupportInitialize).EndInit
-		Me.ResumeLayout(false)
-		Me.PerformLayout
-	End Sub
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(About))
+        Me.pictureBox1 = New System.Windows.Forms.PictureBox
+        Me.label1 = New System.Windows.Forms.Label
+        Me.labelVersion = New System.Windows.Forms.Label
+        Me.labelCopyright = New System.Windows.Forms.Label
+        Me.buttonOK = New System.Windows.Forms.Button
+        Me.linkLabelHomepage = New System.Windows.Forms.LinkLabel
+        CType(Me.pictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pictureBox1
+        '
+        resources.ApplyResources(Me.pictureBox1, "pictureBox1")
+        Me.pictureBox1.Name = "pictureBox1"
+        Me.pictureBox1.TabStop = False
+        '
+        'label1
+        '
+        resources.ApplyResources(Me.label1, "label1")
+        Me.label1.Name = "label1"
+        '
+        'labelVersion
+        '
+        resources.ApplyResources(Me.labelVersion, "labelVersion")
+        Me.labelVersion.Name = "labelVersion"
+        '
+        'labelCopyright
+        '
+        resources.ApplyResources(Me.labelCopyright, "labelCopyright")
+        Me.labelCopyright.Name = "labelCopyright"
+        '
+        'buttonOK
+        '
+        resources.ApplyResources(Me.buttonOK, "buttonOK")
+        Me.buttonOK.Name = "buttonOK"
+        Me.buttonOK.UseVisualStyleBackColor = True
+        '
+        'linkLabelHomepage
+        '
+        resources.ApplyResources(Me.linkLabelHomepage, "linkLabelHomepage")
+        Me.linkLabelHomepage.Name = "linkLabelHomepage"
+        Me.linkLabelHomepage.TabStop = True
+        '
+        'About
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.pictureBox1)
+        Me.Controls.Add(Me.linkLabelHomepage)
+        Me.Controls.Add(Me.buttonOK)
+        Me.Controls.Add(Me.labelCopyright)
+        Me.Controls.Add(Me.labelVersion)
+        Me.Controls.Add(Me.label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "About"
+        Me.ShowInTaskbar = False
+        CType(Me.pictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
 	Private labelVersion As System.Windows.Forms.Label
 	Private labelCopyright As System.Windows.Forms.Label
-	Private linkLabelHomepage As System.Windows.Forms.LinkLabel
-	Private buttonOK As System.Windows.Forms.Button
+	Private WithEvents linkLabelHomepage As System.Windows.Forms.LinkLabel
+	Private WithEvents buttonOK As System.Windows.Forms.Button
 	Private label1 As System.Windows.Forms.Label
 	Private pictureBox1 As System.Windows.Forms.PictureBox
 End Class
--- About.vb
+++ About.vb
@@ -11,36 +11,42 @@ Imports System.Xml
 Imports System.Globalization
 Imports System.Threading
 
-Public Partial Class About
-	Public Sub New()
-		' The Me.InitializeComponent call is required for Windows Forms designer support.
-		Me.InitializeComponent()
-		
-		'
-		' TODO : Add constructor code after InitializeComponents
-		'
-	End Sub
-	
-	Sub LinkLabelHomepageLinkClicked(ByVal sender As Object, ByVal e As LinkLabelLinkClickedEventArgs)
-		Start("http://code.google.com/p/netprofiles/")
-	End Sub
-	
-	Sub ButtonOKClick(ByVal sender As Object, ByVal e As EventArgs)
-		Me.Dispose
-	End Sub
-	
-	Sub AboutLoad(ByVal sender As Object, ByVal e As EventArgs)
-		Call LoadLanguage
-	End Sub
-	
-	Public Sub LoadLanguage()
-		Thread.CurrentThread.CurrentCulture = New CultureInfo(Globals.CurrentLang, False)
-		
-		Dim xDoc As New XmlDocument
-		xDoc.Load(Globals.CurrentLangPath)
-		Dim root As XmlElement = xDoc.DocumentElement
 
-		Me.Text = root.SelectSingleNode("/Language/About/WindowTitle").InnerText.Replace("%1", Globals.ProgramName)
-		Me.buttonOK.Text = root.SelectSingleNode("/Language/About/buttonOK").InnerText
-	End Sub
+Public Partial Class About
+
+    Public Sub New()
+        ' The Me.InitializeComponent call is required for Windows Forms designer support.
+        Me.InitializeComponent()
+
+        '
+        ' TODO : Add constructor code after InitializeComponents
+        ''
+    End Sub
+
+    Sub ButtonOKClick(ByVal sender As Object, ByVal e As EventArgs) Handles buttonOK.Click
+        Me.Dispose()
+    End Sub
+
+    Sub AboutLoad(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Call LoadLanguage()
+    End Sub
+
+    Public Sub LoadLanguage()
+        Thread.CurrentThread.CurrentCulture = New CultureInfo(Globals.CurrentLang, False)
+
+        Dim xDoc As New XmlDocument
+        xDoc.Load(Globals.CurrentLangPath)
+        Dim root As XmlElement = xDoc.DocumentElement
+
+        Me.Text = root.SelectSingleNode("/Language/About/WindowTitle").InnerText.Replace("%1", Globals.ProgramName)
+        Me.buttonOK.Text = root.SelectSingleNode("/Language/About/buttonOK").InnerText
+    End Sub
+
+    Private Sub linkLabelHomepage_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles linkLabelHomepage.LinkClicked
+        Start("http://code.google.com/p/netprofiles/")
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+    End Sub
 End Class
