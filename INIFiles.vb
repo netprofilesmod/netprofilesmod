384e3cfbd360c52ff9d465d517a6f082dec062a5
dmilner@gmail.com <dmilner@gmail.com@5099ddc8-4009-11de-952f-710615d9d0d5>
2011-05-17 19:37:57 +0000

    Fixed some conversion type errors.
    git-svn-id: http://netprofiles.googlecode.com/svn/trunk@24 5099ddc8-4009-11de-952f-710615d9d0d5

--- INIFiles.vb
+++ INIFiles.vb
@@ -24,47 +24,47 @@ Module INIFiles
 		ByVal lpFileName As String) As Int32
 	#End Region
 
-	Public Overloads Function INIRead(ByVal INIPath As String, ByVal SectionName As String, ByVal KeyName As String, ByVal DefaultValue As String) As String
-		' primary version of call gets single value given all parameters
-		Dim n As Int32
-		Dim sData As String
-		sData = space$(1024) ' allocate some room
-		n = GetPrivateProfileString(SectionName, KeyName, DefaultValue, _
-			sData, sData.Length, INIPath)
-		If n > 0 Then ' return whatever it gave us
-			INIRead = sdata.Substring(0, n)
-		Else
-			iniread = ""
-		End If
-	End Function
+    Public Overloads Function INIRead(ByVal INIPath As String, ByVal SectionName As String, ByVal KeyName As String, ByVal DefaultValue As String) As String
+        ' primary version of call gets single value given all parameters
+        Dim n As Int32
+        Dim sData As String
+        sData = Space$(1024) ' allocate some room
+        n = GetPrivateProfileString(SectionName, KeyName, DefaultValue, _
+         sData, sData.Length, INIPath)
+        If n > 0 Then ' return whatever it gave us
+            INIRead = sData.Substring(0, n)
+        Else
+            INIRead = ""
+        End If
+    End Function
 
-	#Region "INIRead Overloads"
-	Public Overloads Function INIRead(ByVal INIPath As String, ByVal SectionName As String, ByVal KeyName As String) As String
-		' overload 1 assumes zero-length default
-		Return INIRead(inipath, sectionname, KeyName, "")
-	End Function
+#Region "INIRead Overloads"
+    Public Overloads Function INIRead(ByVal INIPath As String, ByVal SectionName As String, ByVal KeyName As String) As String
+        ' overload 1 assumes zero-length default
+        Return INIRead(INIPath, SectionName, KeyName, "")
+    End Function
 
-	Public Overloads Function INIRead(ByVal INIPath As String, ByVal SectionName As String) As String
-		' overload 2 returns all keys in a given section of the given file
-		Return INIRead(inipath, sectionname, Nothing, "")
-	End Function
+    Public Overloads Function INIRead(ByVal INIPath As String, ByVal SectionName As String) As String
+        ' overload 2 returns all keys in a given section of the given file
+        Return INIRead(INIPath, SectionName, Nothing, "")
+    End Function
 
-	Public Overloads Function INIRead(ByVal INIPath As String) As String
-		' overload 3 returns all section names given just path
-		Return INIRead(inipath, Nothing, Nothing, "")
-	End Function
-	#End Region
+    Public Overloads Function INIRead(ByVal INIPath As String) As String
+        ' overload 3 returns all section names given just path
+        Return INIRead(INIPath, Nothing, Nothing, "")
+    End Function
+#End Region
 
-	Public Sub INIWrite(ByVal INIPath As String, ByVal SectionName As String, ByVal KeyName As String, ByVal TheValue As String)
-		Call WritePrivateProfileString(SectionName, KeyName, TheValue, INIPath)
-	End Sub
+    Public Sub INIWrite(ByVal INIPath As String, ByVal SectionName As String, ByVal KeyName As String, ByVal TheValue As String)
+        Call WritePrivateProfileString(SectionName, KeyName, TheValue, INIPath)
+    End Sub
 
-	Public Overloads Sub INIDelete(ByVal INIPath As String, ByVal SectionName As String, ByVal KeyName As String) ' delete single line from section
-		Call WritePrivateProfileString(SectionName, KeyName, Nothing, INIPath)
-	End Sub
+    Public Overloads Sub INIDelete(ByVal INIPath As String, ByVal SectionName As String, ByVal KeyName As String) ' delete single line from section
+        Call WritePrivateProfileString(SectionName, KeyName, Nothing, INIPath)
+    End Sub
 
-	Public Overloads Sub INIDelete(ByVal INIPath As String, ByVal SectionName As String)
-		' delete section from INI file
-		Call WritePrivateProfileString(SectionName, Nothing, Nothing, INIPath)
-	End Sub
+    Public Overloads Sub INIDelete(ByVal INIPath As String, ByVal SectionName As String)
+        ' delete section from INI file
+        Call WritePrivateProfileString(SectionName, Nothing, Nothing, INIPath)
+    End Sub
 End Module
