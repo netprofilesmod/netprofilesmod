4b49a5b854f3d5cbfe9d23bd42cc21720013ef62
Urs Geissbühler <urx@urx.ch>
2012-12-07 20:03:02 +0100

    Profile file operations on XP now only showing the Run As dialog if permissions on profiles folder are missing. UAC conform operation on Vista and newer is now achieved, on XP the equivalent Run As dialog will be displayed. (Fixes issue 28)

--- Net Profiles mod/ProfileWriter.vb
+++ Net Profiles mod/ProfileWriter.vb
@@ -25,32 +25,81 @@
 ' 
 ' To change this template use Tools | Options | Coding | Edit Standard Headers.
 '
+Imports AppModule.Globals
+
 Public Module ProfileWriter
 	Function SaveProfile(SourceFile As String, TargetFile As String) As Boolean
 		Try
-			Return ElevatedProcess("move """ & SourceFile & """ """ & TargetFile & """")
+			Dim osInfo As System.OperatingSystem = System.Environment.OSVersion
+			If osInfo.Version.Major < 6 Then
+				' On XP try to save the profile without raising a Run As dialog
+				' This will fail if the user is not an administrator
+				FileopSaveProfile(SourceFile, TargetFile)
+				Return True
+			Else
+				' On Vista or newer, with UAC enabled, prevent writting the profile to the VirtualStore
+				Throw New Exception()
+			End If
 		Catch
-			Return False
+			Try
+				' Using the elevated process for saving the profile ensures write access to the profiles folder
+				Return ElevatedProcess("move """ & SourceFile & """ """ & TargetFile & """")
+			Catch
+				Return False
+			End Try
 		End Try
 	End Function
 	
 	Function RemoveProfile(TargetFile As String) As Boolean
 		Try
-			ElevatedProcess("delete """ & TargetFile & """")
-			Return True
-		Catch
-			Return False
+			Dim osInfo As System.OperatingSystem = System.Environment.OSVersion
+			If osInfo.Version.Major < 6 Then
+				' On XP try to delete the profile without raising a Run As dialog
+				FileopDelete(TargetFile)
+				Return True
+			Else
+				' On Vista or newer, with UAC enabled, deleting the profile would fail
+				Throw New Exception()
+			End If
+		Catch	
+			Try
+				' Using the elevated process for saving the profile ensures write access to the profiles folder
+				ElevatedProcess("delete """ & TargetFile & """")
+				Return True
+			Catch
+				Return False
+			End Try
 		End Try
 	End Function
 	
 	Function ElevatedProcess(Arguments As String) As Boolean
+		' Starts an elevated copy of the current process with command line parameters for write access to the
+		' protected profiles folder
 		Dim FileOpsProcess As New Process()
-		FileOpsProcess.StartInfo.FileName = System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe"
+		' Path of current executable
+		FileOpsProcess.StartInfo.FileName = System.Reflection.Assembly.GetEntryAssembly().Location
 		FileOpsProcess.StartInfo.Arguments = "profileops " & Arguments
-		' Start an elevated process
+		' Start the process elevated
 		FileOpsProcess.StartInfo.Verb = "runas"
 		FileOpsProcess.Start()
 		Return FileOpsProcess.WaitForExit(5000)
 		'TODO: Return the exit code, raise an exception if WaitForExit() returns False
 	End Function
+	
+	Sub FileopSaveProfile(SourceFile As String, TargetFile As String)
+		Dim Target() As String = TargetFile.Split(CChar("\"))
+		Dim TargetFolder As String = Target(Target.Length - 2)
+		' Make sure the subfolder named after the MAC address exists
+		If Not System.IO.Directory.Exists(ProfilesFolder + "\" + TargetFolder)  Then
+			MkDir(ProfilesFolder + "\" + TargetFolder)
+		End If
+		' Just moving the profile would keep the write permissions for the user and allow him to modify the profile
+		' without administrator permissions, therfore it's copied from the temp folder and then deleted
+		System.IO.File.Copy(SourceFile, TargetFile, True)
+		System.IO.File.Delete(SourceFile)
+	End Sub
+	
+	Sub FileopDelete(TargetFile As String)
+		System.IO.File.Delete(TargetFile)
+	End Sub	
 End Module
